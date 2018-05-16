3d2c97335abdc1d0df8f4e54519cdc6e52c76678
Bruno Giuseppe Moretto <bruno.bgmbgm@gmail.com>
2018-05-16 01:22:36 -0300

    BowlMaster

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public bool inPlay = false;
+    public float launchSpped;
+    public Vector3 launchVelocity;
+
+    private Rigidbody rigidbody;
+    private AudioSource audioSource;
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+
+        //Launch(launchVelocity);
+    }
+
+    public void Launch(Vector3 velocity)
+    {
+        inPlay = true;
+
+        rigidbody.useGravity = true;
+        rigidbody.velocity = velocity;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.Play();
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour {
+
+    private Ball ball;
+    private Vector3 dragStart, dragEnd;
+    private float startTime, endTime;
+
+	// Use this for initialization
+	void Start () {
+        ball = GetComponent<Ball>();
+	}
+	
+    public void MoveStart(float amount)
+    {
+        //Debug.Log("Ball moved " + amount);
+        if (!ball.inPlay)
+        {
+            ball.transform.Translate(new Vector3(amount, 0, 0));
+        }
+
+    }
+
+    public void DragStart()
+    {
+        //Capture time & position of drag start
+        dragStart = Input.mousePosition;
+        startTime = Time.time;
+
+    }
+
+    public void DragEnd()
+    {
+        // Launch the ball
+        dragEnd = Input.mousePosition;
+        endTime = Time.time;
+
+        float duration = endTime - startTime;
+
+        float launchSpeedX = (dragEnd.x - dragStart.x) / duration;
+        float launchSpeedZ = (dragEnd.y - dragStart.y) / duration;
+        Vector3 launchVelocity = new Vector3(launchSpeedX, 0, launchSpeedZ);
+        ball.Launch(launchVelocity);
+    }
+	
+}
