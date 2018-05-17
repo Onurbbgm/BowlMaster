e55fa557dbe1711baead06091708ba466ecec39b
Bruno Giuseppe Moretto <bruno.bgmbgm@gmail.com>
2018-05-16 22:54:10 -0300

    contar pinos

---
+++ Assets/Scripts/Pin.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour {
+
+    public float standingThreshold = 10f;
+
+    private void Awake()
+    {
+        this.GetComponent<Rigidbody>().solverVelocityIterations = 10;
+    }
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(name + IsStanding());
+    }
+
+    public bool IsStanding()
+    {
+        //print(transform.rotation.eulerAngles);
+
+        Vector3 rotationInEuler = transform.rotation.eulerAngles;
+        float tiltInX = Mathf.Abs(rotationInEuler.x);
+        float tiltInZ = Mathf.Abs(rotationInEuler.z);
+        //print(tiltInX +" "+ tiltInZ);
+
+        if(tiltInX < standingThreshold && tiltInZ < standingThreshold)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
